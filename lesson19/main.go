573e11cea572feaee8b08751a4fa16695a0a8c9b
takahirokaji <ca01965@cartahd.com>
2024-02-12 15:38:28 +0900

    .

--- lesson19/main.go
+++ lesson19/main.go
@@ -1,29 +1,4 @@
 package main
 
-import "fmt"
-
 func main() {
-	var x interface{}
-	//すべての方を汎用的に表すもの
-	// すべての型と互換性がある
-	// 初期値としてnilを持っている
-
-	// 初期値は nil
-	fmt.Println(x)
-
-	x = 1
-	fmt.Println(x)
-
-	x = 3.14
-	fmt.Println(x)
-
-	x = "A"
-	fmt.Println(x)
-
-	x = [3]int{1, 2, 3}
-	fmt.Println(x)
-
-	//x = 2
-	// これはだめです
-	//fmt.Println(x + 3)
 }
--- lesson22/main.go
+++ lesson22/main.go
@@ -1,4 +1,13 @@
 package main
 
+import "fmt"
+
+// 型変換
+// 型キャストっていえよ
+// これは流し見
+
 func main() {
+	var i int = 1
+	fl64 := float64(i)
+	fmt.Println(fl64)
 }
